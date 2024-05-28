ac75e5837c1b181a41928ff9f48485eeafc50236
samymahmoud3 <samymahmoud2532001@gmail.com>
2024-05-28 21:24:37 +0300

    hero slider

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -44,7 +44,7 @@ const NavIcons = () => {
         onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="absolute p-3 top-12 -left-5 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+        <div className="absolute p-3 top-12 bg-white -left-5 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href='/'>Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
